bf63a20ca67c4fd5d7f17d65a0748ae7235b748f
David <davidf14@ucm.es>
2020-02-12 19:10:59 +0100

    Añadidos nuevos atributos y metodos en algunas clases.

--- src/simulator/model/RoadMap.java
+++ src/simulator/model/RoadMap.java
@@ -1,4 +1,18 @@
 package simulator.model;
 
+import java.util.List;
+import java.util.Map;
+
 public class RoadMap {
+	protected  List<Junction> JunctionList;
+	protected List<Road> RoadList; 
+	protected List<Vehicle> VehicleList;
+	protected Map<String,Junction> IdJunctionMap;
+	protected Map<String,Road> IdRoadMap;
+	protected Map<String,Vehicle> IdVehicleMap;
+	
+	
+	
+	
+	
 }
