fd39eee334caab3a32ce86c27766bce54ed534d7
David <davidf14@ucm.es>
2020-02-17 18:10:03 +0100

    Añadidas nuevas clases y nueva funcionalidad en la clase vehiculo

--- src/simulator/model/RoadMap.java
+++ src/simulator/model/RoadMap.java
@@ -3,16 +3,81 @@ package simulator.model;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONObject;
+
 public class RoadMap {
-	protected  List<Junction> JunctionList;
-	protected List<Road> RoadList; 
-	protected List<Vehicle> VehicleList;
+	private  List<Junction> JunctionList;
+	private List<Road> RoadList; 
+	private List<Vehicle> VehicleList;
 	protected Map<String,Junction> IdJunctionMap;
 	protected Map<String,Road> IdRoadMap;
 	protected Map<String,Vehicle> IdVehicleMap;
 	
+	protected void addJunction(Junction j) {
+		
+	}
+	
+	
+	protected void addRoad(Road r) {
+		
+	}
+	
+	protected void addVehicle(Vehicle v) {
+		
+	}
+	
+	public Junction getJunction(String id) {
+		return null; 
+	}
+	
+	public Road RoadGetRoad(String id) {
+		return null; 
+	}
+
+	public Vehicle getVehicle(String id) {
+		return null; 
+	}
+
+
+	public List<Junction> getJunctionList() {
+		return JunctionList;
+	}
+
+
+	public void setJunctionList(List<Junction> junctionList) {
+		JunctionList = junctionList;
+	}
+
+
+	public List<Road> getRoadList() {
+		return RoadList;
+	}
+
+
+	public void setRoadList(List<Road> roadList) {
+		RoadList = roadList;
+	}
+
+
+	public List<Vehicle> getVehicleList() {
+		return VehicleList;
+	}
+
+
+	public void setVehicleList(List<Vehicle> vehicleList) {
+		VehicleList = vehicleList;
+	}
+	
+	protected void reset() {
+		
+	}
+	
+	public JSONObject report() {
+		return null;
+	}
 	
 	
 	
+
 	
 }
