cc60248e3005be3736fb21d7250bf9ace1387d6f
David <davidf14@ucm.es>
2020-03-18 13:29:22 +0100

    Mejorado con herencia

--- src/simulator/model/NewCityRoadEvent.java
+++ src/simulator/model/NewCityRoadEvent.java
@@ -4,18 +4,6 @@ import Exceptions.InvalidArgumentException;
 
 public class NewCityRoadEvent  extends NewRoadEvent{
 	
-	private int time; 
-	private String id; 
-	private String srcJunc; 
-	private String destJunc; 
-	private int length; 
-	private int co2Limit;
-	private int maxSpeed; 
-	private Weather weather ;
-	
-	
-	private Junction src ;
-	private Junction dest; 
 
 	
 	public NewCityRoadEvent(int time ,String id,String srcJun ,String destJunc,int length,int co2Limit , int maxSpeed , Weather weather ) {
@@ -66,70 +54,6 @@ public class NewCityRoadEvent  extends NewRoadEvent{
 	}
 	
 	
-	
-	public int getTime() {
-		return time;
-	}
-
-	public void setTime(int time) {
-		this.time = time;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getSrcJunc() {
-		return srcJunc;
-	}
-
-	public void setSrcJunc(String srcJunc) {
-		this.srcJunc = srcJunc;
-	}
-
-	public String getDestJunc() {
-		return destJunc;
-	}
-
-	public void setDestJunc(String destJunc) {
-		this.destJunc = destJunc;
-	}
-
-	public int getLength() {
-		return length;
-	}
-
-	public void setLength(int length) {
-		this.length = length;
-	}
-
-	public int getCo2Limit() {
-		return co2Limit;
-	}
-
-	public void setCo2Limit(int co2Limit) {
-		this.co2Limit = co2Limit;
-	}
-
-	public int getMaxSpeed() {
-		return maxSpeed;
-	}
-
-	public void setMaxSpeed(int maxSpeed) {
-		this.maxSpeed = maxSpeed;
-	}
-
-	public Weather getWeather() {
-		return weather;
-	}
-
-	public void setWeather(Weather weather) {
-		this.weather = weather;
-	}
-}
+} 
 
 	
